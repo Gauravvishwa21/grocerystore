062cd4214ffa2f38601b064c3f5a7d49e28ec1d7
Gaurav Vishwa <67256537+Gauravvishwa21@users.noreply.github.com>
2021-08-27 11:11:32 +0530

    Add files via upload

---
+++ about_us_footer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		About Us
+	</title>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/about_style.css">
+	<link rel="stylesheet" href="bootstrap\bootstrap.min.css">
+	    <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<!--About Us-->
+<div class="team-section">
+	<h1>Our Team</h1>
+	<span class=border></span>
+	<div class="ps">
+		<a href="#p1"><img src="image/Mukesh.jpeg" alt=""></a>
+		<a href="#p2"><img src="Image/ME.jpg" alt=""></a>
+		<a href="#p3"><img src="image/sominto%20.jpg" alt="" style="margin-top:-40px"></a>
+		<a href="#p4"><img src="image/rahul%20(2).jpg" alt=""></a>
+	</div>
+
+<div class="section " id="p1">
+	<span class="name">Mukesh</span>
+	<span class="border"></span>
+	<p>
+	Hi ! Myself Mukesh kumar and I played role as a front-end designer and market manager. I do have knowledge in web designing as well as marketing. As it is an ecommerce website, so its economical feasibility is properly studied and then get structured this project. I used HTML, CSS, html5,Bootstrap for layout designing.
+	</p>
+
+</div>
+<div class="section " id="p2">
+	<span class="name">Divya</span>
+	<span class="border"></span>
+	<p>
+	 Hello viewers! Myself Divya Jha and I worked as a web designer and web developer. I have code the from-end and back-end to make the website workable.
+	 I used PHP, javascript, ajax to make the website responsible. Also helped in planning and directing the project work. For back-end MySQL is used. I can do web site development, ecommerce website, real estate website, travel web development and much more.
+	</p>
+
+</div>
+<div class="section " id="p3">
+	<span class="name">Sominto</span>
+	<span class="border"></span>
+	<p>
+	Hey Its Sominto Charlie there! I contribute as a front-end designer and project manager. I do have a good knowledge in web design. I used HTML, CSS, html5,Bootstrap for layout designing.Also I perform a managerial role during project development. I tried to attached few of my knowledge in this project.I can do website design, logo design, banner design and much more.	</p>
+
+</div>
+<div class="section " id="p4">
+	<span class="name">Rahul</span>
+	<span class="border"></span>
+	<p>
+	Hello! Myself Rahul and I worked as a database designer and content collector. Also I have prepared documentation for this project. The content selection is a very tedious and creative task to make a website impressive. I have supervised the image collection for the data keeping and desined the database by using DFD and physical structure.	</p>
+
+</div>
+
+
+
+
+
+</div>
+</body>
+</html>
